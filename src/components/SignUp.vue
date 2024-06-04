5b478c8e8eec07d0a00227e0e26c4bf1c1e2c788
saetgit <etemadian.sara.1998@gmail.com>
2024-06-04 13:34:21 +0330

    add function for signup form

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,22 +3,43 @@
         <img src="../assets/logo.jpg" class="logo" />
         <h1>ثبت نام</h1>
         <div class="register">
-            <input type="text" placeholder="لطفا نام خود را وارد کنید" />
-            <input type="text" placeholder="لطفا ایمیل خود را وارد کنید" />
-            <input type="password" placeholder="لطفا رمزعبور خود را وارد کنید" />
-            <button>ثبت نام</button>
+            <input type="text" v-model="user.name" placeholder="لطفا نام خود را وارد کنید" />
+            <input type="text" v-model="user.email" placeholder="لطفا ایمیل خود را وارد کنید" />
+            <input type="password" v-model="user.password" placeholder="لطفا رمزعبور خود را وارد کنید" />
+            <button @click="signUp">ثبت نام</button>
         </div>
     </div>
 </template>
 
-<script>
+<script setup>
+import { reactive } from 'vue'
+import axios from 'axios'
+const user = reactive({
+    name: "",
+    email: "",
+    password: ""
+});
+
+
+const signUp = async () => {
+    let result = await axios.post("http://localhost:3000/users", {
+        email: user.email,
+        password: user.password,
+        name: user.name
+    })
+    if(result.status==201){
+        alert("signUp-done!");
+        localStorage.setItem("user-info",JSON.stringify(result.data))
+    }
+}
 
 </script>
 
 <style scoped>
-.signup{
+.signup {
     text-align: center;
 }
+
 .logo {
     width: 100px;
 }
