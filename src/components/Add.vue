56e9730df4f0cf89ae8edb87aebdd3ef80b15c04
saetgit <etemadian.sara.1998@gmail.com>
2024-06-06 17:33:24 +0330

    add restaurant in api

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -2,9 +2,9 @@
     <div>
         <Header />
         <form class="Add">
-            <input type="text" name="name" placeholder="نام رستوران را وارد کنید" v-model="restaurant.name"/>
-            <input type="text" name="address" placeholder="آدرس رستوران را وارد کنید" v-model="restaurant.address"/>
-            <input type="text" name="contact" placeholder="شماره رستوران را وارد کنید" v-model="restaurant.contact"/>
+            <input type="text" name="name" placeholder="نام رستوران را وارد کنید" v-model="restaurant.name" />
+            <input type="text" name="address" placeholder="آدرس رستوران را وارد کنید" v-model="restaurant.address" />
+            <input type="text" name="contact" placeholder="شماره رستوران را وارد کنید" v-model="restaurant.contact" />
             <button type="button" @click="addRestaurant">افزودن رستوران</button>
         </form>
     </div>
@@ -12,8 +12,9 @@
 
 <script setup>
 import Header from './Header.vue'
-import { onMounted,reactive } from 'vue'
+import { onMounted, reactive } from 'vue'
 import { useRouter } from 'vue-router'
+import axios from 'axios'
 const router = useRouter()
 const restaurant = reactive({
     name: '',
@@ -25,7 +26,15 @@ components: {
 }
 
 
-const addRestaurant = () => {
+const addRestaurant = async () => {
+    let result = await axios.post("http://localhost:3000/restaurant", {
+        name: restaurant.name,
+        address: restaurant.address,
+        contact: restaurant.contact
+    });
+    if (result.status == 201) {
+        router.push({ name: 'Home' })
+    }
 }
 onMounted(() => {
     let user = localStorage.getItem('user-info')
